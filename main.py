10a634277f59527e075007a199e1d5a9f4237277
Thiago William <117531207+thiagowmoreira@users.noreply.github.com>
2024-07-29 17:41:55 -0300

    alguns ajustes de linhas comentadas

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import pandas
+
 student_dict = {
     "student": ["Angela", "James", "Lily"], 
     "score": [56, 76, 98]
@@ -8,18 +10,17 @@ for (key, value) in student_dict.items():
     #Access key and value
     pass
 
-import pandas
+
 student_data_frame = pandas.DataFrame(student_dict)
 
 #Loop through rows of a data frame
 for (index, row) in student_data_frame.iterrows():
-    #Access index and row
-    #Access row.student or row.score
+#Access index and row
+#Access row.student or row.score
     pass
 
 # Keyword Method with iterrows()
 # {new_key:new_value for (index, row) in df.iterrows()}
-
 
 nato_df = pandas.read_csv("nato_phonetic_alphabet.csv")
 
@@ -27,12 +28,7 @@ nato_df = pandas.read_csv("nato_phonetic_alphabet.csv")
 nato_dict = {row.letter: row.code for (key, row) in nato_df.iterrows()}
 print(nato_dict)
 
-
-
 name = input("tel me your name and I will NATO it: ").upper()
 nato_name = [nato_dict[letter] for letter in name if letter.isalpha()]
 
 print(nato_name)
-
-
-
