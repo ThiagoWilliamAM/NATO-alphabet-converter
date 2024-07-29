7b9c6caef5e4dfb0783a4e95a751574cd27dc124
Thiago William <117531207+thiagowmoreira@users.noreply.github.com>
2024-07-29 17:52:45 -0300

    ajuste final das linhas comentadas

--- main.py
+++ main.py
@@ -1,26 +1,31 @@
 import pandas
 
+"""linhas para relembrar uso de dict e dataframe"""
 student_dict = {
     "student": ["Angela", "James", "Lily"], 
     "score": [56, 76, 98]
 }
 
-#Looping through dictionaries:
-for (key, value) in student_dict.items():
-    #Access key and value
-    pass
+# # Looping through dictionaries:
+# for (key, value) in student_dict.items():
+#     # Access key and value
+#     pass
 
 
 student_data_frame = pandas.DataFrame(student_dict)
 
-#Loop through rows of a data frame
-for (index, row) in student_data_frame.iterrows():
-#Access index and row
-#Access row.student or row.score
-    pass
+# Loop through rows of a data frame
+# for (index, row) in student_data_frame.iterrows():
+#     Access index and row
+#     Access row.student or row.score
+
 
 # Keyword Method with iterrows()
 # {new_key:new_value for (index, row) in df.iterrows()}
+
+"""fim do relembrar"""
+
+"""inicio da atividade"""
 
 nato_df = pandas.read_csv("nato_phonetic_alphabet.csv")
 
